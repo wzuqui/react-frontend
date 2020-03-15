0fb10631402a0d0c935471668e4a1010429782ab
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2020-03-15 01:00:34 -0300

    feat: rotas dinamicase lazy

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Cabecalho from './components/Cabecalho';
+import usePersistedState from './hooks/usePersistedState';
 import NovaAba from './pages/NovaAba';
+import { Menu } from './services/Menu';
 import { GlobalStyles } from './styles/global';
 import dark from './styles/themes/dark';
 import light from './styles/themes/light';
-import usePersistedState from './hooks/usePersistedState';
 
-function App() {
+interface IAppProps {}
+
+const App: React.FC<IAppProps> = props => {
   const [theme, setTheme] = usePersistedState('theme', light);
 
   const toggleTheme = () => {
@@ -34,10 +37,18 @@ function App() {
           <Route exact path="/">
             <NovaAba />
           </Route>
+          {Menu.obterLista().map(menu => {
+            const Page = React.lazy(() => import('./pages/' + menu.pasta));
+            return (
+              <Route key={menu.rota} path={`/${menu.rota}`}>
+                <Page></Page>
+              </Route>
+            );
+          })}
         </Switch>
       </Router>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { initializeIcons } from '@uifabric/icons';
 import { setIconOptions } from '@uifabric/styling';
 
 import 'devextreme/dist/css/dx.common.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
@@ -13,4 +13,7 @@ setIconOptions({
   disableWarnings: true
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Suspense fallback={<div>Carregando...</div>}>
+  <App />
+</Suspense>, document.getElementById('root'));
