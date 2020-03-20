a212da5b70fc0f609838d3d99a3299958152b323
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2020-03-20 19:08:58 -0300

    Logo em modo dark

--- src/pages/NovaAba/Logo/index.tsx
+++ src/pages/NovaAba/Logo/index.tsx
@@ -1,17 +1,22 @@
-import * as React from 'react';
-import styled from 'styled-components';
+import React, { useContext } from 'react';
+import styled, { ThemeContext } from 'styled-components';
 
 const LogoComponent: React.FunctionComponent<{
   className?: string;
-}> = props => (
-  <div className={props.className}>
-    <img
-      src="http://sistema.ravex.com.br/assets/images/ravex.svg"
-      height="100px"
-      alt="Ravex"
-    />
-  </div>
-);
+}> = props => {
+  const theme = useContext(ThemeContext);
+
+  const src =
+    theme.titulo === 'light'
+      ? 'http://sistema.ravex.com.br/assets/images/ravex.svg'
+      : 'http://sistema.ravex.com.br/assets/images/logo-no-text-prod.svg';
+
+  return (
+    <div className={props.className}>
+      <img width="310px" alt="Ravex" src={src} />
+    </div>
+  );
+};
 
 export const Logo = styled(LogoComponent)`
   padding-top: 20px;
